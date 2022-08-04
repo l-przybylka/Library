63a224a146d5e586f8548f79954564ce415cbf5b
Lukasz <lukaszprzybylka@gmail.com>
2022-08-04 08:10:27 +0100

    test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var mongoose = require('mongoose')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,7 +9,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// default mongo connection
+var mongoDB = 'mongodb://127.0.0.1/my_database'
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 
+var db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  next()
 });
+
+router.get('/cool', (req,res) => {
+  res.send("You're so cool!")
+})
 
 module.exports = router;
